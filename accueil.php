84c6dac4c6d1429627342e2353550f0c13beff13
Jérémy CHANG <jchan@lmdsio.com>
2024-09-18 15:30:12 +0200

    exo 5 ver 2

--- accueil.php
+++ accueil.php
@@ -1,19 +1,28 @@
-<head>
-    <meta charset="UTF-8" />
-</head>
-<body>
-<p style="text-align: center" >
-    <?php
-    if((isset($_POST["nom"]) && !empty($_POST["nom"])) or (isset($_POST["prenom"]) && !empty($_POST["prenom"]))) {
-
-// récupération des zones de saisie dans des variables locales
-        $nom = $_POST["nom"];
-        $prenom = $_POST["prenom"];
-
-
-        echo 'Bienvenu ' . $_POST["nom"] .' '. $_POST["prenom"];
-
-    }
-    ?>
-</p>
-</body>
+Bonjour <?php echo $_POST["prenom"]." ".$_POST['nom']; ?>
+<?php
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+if ($_POST["login"]!="admin" && $_POST["mdp"]!="azerty") {
+    $extra = 'profil.html';
+    header("Location: http://$host$uri/$extra");
+    exit;
+}else{
+    $extra = 'profil.html';
+    header("Location: http://$host$uri/$extra");
+    exit;
+}
+?>
+Bonjour <?php echo $_POST["prenom"]." ".$_POST['nom']; ?>
+<?php
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+if ($_POST["login"]=="admin" && $_POST["mdp"]=="azerty") {
+    $extra = 'index.html';
+    header("Location: http://$host$uri/$extra");
+    exit;
+}else{
+    $extra = 'index.html';
+    header("Location: http://$host$uri/$extra");
+    exit;
+}
+?>
